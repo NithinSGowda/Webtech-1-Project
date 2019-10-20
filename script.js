f75cf585852df69d222db1a0374f51225b53173c
NithinSGowda <nithin.s491@gmail.com>
2019-10-20 15:45:25 +0530

    Added movable elements inside the blue area

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const elements=document.querySelectorAll('.et');
 
 for(element of elements){
-element.addEventListener("click",function(){
+element.addEventListener("dblclick",function(){
     const editedText=prompt("Enter the Heading");
     this.innerHTML=editedText;
 });}
@@ -33,3 +33,56 @@ function download(e){
 }
 
 
+
+for(element of elements)
+{dragElement(element);}
+
+
+
+
+
+
+
+function dragElement(elmnt) {
+  
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id)) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id).onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    elmnt.style.position="absolute";
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
